792135daca19b047f3f7ba836ed95806de0dbc32
Iwan BK <iwanbk@gmail.com>
2013-12-30 10:43:09 +0700

    API Exception. contains error details from API server

---
+++ src/main/java/com/plivo/helper/exception/APIException.java
@@ -0,0 +1,35 @@
+package com.plivo.helper.exception;
+
+public class APIException extends Exception {
+	private static final long serialVersionUID = 1L;
+	private final String message;
+	private final int serverCode;
+	private final String error;
+
+	public APIException(int serverCode, String message, String error) {
+		super();
+		this.serverCode = serverCode;
+		this.message = message;
+		this.error = error;
+	}
+
+	@Override
+	public String toString() {
+		return "Plivo API Exception. Server Code = " + serverCode
+				+ ".message = " + message + ".error = " + error;
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+
+	public int getServerCode() {
+		return serverCode;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+}
