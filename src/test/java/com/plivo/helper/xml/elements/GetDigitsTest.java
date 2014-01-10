0ae2ae76131446553adf7958543ee13d2d347a52
Iwan BK <iwanbk@gmail.com>
2014-01-10 10:25:40 +0700

    add getdigits test

--- src/test/java/com/plivo/helper/xml/elements/GetDigitsTest.java
+++ src/test/java/com/plivo/helper/xml/elements/GetDigitsTest.java
@@ -30,11 +30,43 @@ public class GetDigitsTest {
 			getdigits.append(speak);
 			plivoXML.append(getdigits);
 
-			String expected = TestUtils.inStreamToString(SpeakTest.class.getResourceAsStream("getdigits.xml"));
+			String expected = TestUtils.inStreamToString(SpeakTest.class
+					.getResourceAsStream("getdigits.xml"));
 
 			assertEquals(expected, plivoXML.toXML());
 		} catch (PlivoException e) {
 			fail(e.getLocalizedMessage());
+		}
+	}
+
+	/**
+	 * Test getDigits element with speak element inside of it
+	 */
+	public void testGetDigitsSpeak() {
+		PlivoXML plivoXML = new PlivoXML();
+
+		GetDigits gd = new GetDigits();
+
+		gd.setAction("http://safe-stream-4972.herokuapp.com/incall/chooselang/getdigits");
+		gd.setMethod("GET");
+		gd.setTimeout(20);
+		gd.setNumDigits(1);
+
+		Speak speak = new Speak(
+				"Welcome to Plivo Demo app. Press 1 to hear welcome in english, press 2 to hear it in french");
+
+		Speak timeout = new Speak(
+				"Sorry, I didn't catch that. Please hangup the call and try again later.");
+		try {
+			gd.append(speak);
+			plivoXML.append(gd);
+			plivoXML.append(timeout);
+			String expected = TestUtils.inStreamToString(SpeakTest.class
+					.getResourceAsStream("getdigits_speak.xml"));
+
+			assertEquals(expected, plivoXML.toXML());
+		} catch (PlivoException pe) {
+			fail(pe.getLocalizedMessage());
 		}
 	}
 }
