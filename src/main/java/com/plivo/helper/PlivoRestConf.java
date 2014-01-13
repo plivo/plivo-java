67ee49af8d32009de6a4aac861b1a1a3e6763042
Iwan BK <iwanbk@gmail.com>
2014-01-13 09:33:19 +0700

    Add another PlivoRestConf constructor and add javadoc

--- src/main/java/com/plivo/helper/PlivoRestConf.java
+++ src/main/java/com/plivo/helper/PlivoRestConf.java
@@ -6,10 +6,34 @@ public class PlivoRestConf {
 	private final String version;
 	private final String apiURL = "https://api.plivo.com";
 
+	/**
+	 * 
+	 * @param authId
+	 *            Plivo API Auth ID
+	 * @param authToken
+	 *            Plivo API Auth token
+	 * @param version
+	 *            Plivo API Version
+	 */
 	public PlivoRestConf(String authId, String authToken, String version) {
 		this.authId = authId;
 		this.authToken = authToken;
 		this.version = version;
+	}
+
+	/**
+	 * Contruct Plivo REST configuration object with default API version.
+	 * Current default API version is v1
+	 * 
+	 * @param authId
+	 *            Plivo API Auth ID
+	 * @param authToken
+	 *            Plivo API Auth token
+	 */
+	public PlivoRestConf(String authId, String authToken) {
+		this.authId = authId;
+		this.authToken = authToken;
+		this.version = "v1";
 	}
 
 	public String getAuthId() {
