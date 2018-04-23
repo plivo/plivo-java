27af6f86616cb79924cc4ee0bef4709fdd0f155c
Abhishek-plivo <abhishek@plivo.com>
2018-04-23 14:17:17 +0530

    CT-82 | add new test cases to for resource.client(client).action() for pricing

--- src/test/java/com/plivo/api/PricingTest.java
+++ src/test/java/com/plivo/api/PricingTest.java
@@ -24,4 +24,21 @@ public class PricingTest extends BaseTest {
 
     assertRequest("GET", "Pricing/", params);
   }
+  @Test
+
+  public void pricingGetWithClientShouldSucceed() throws Exception {
+    expectResponse("pricingGetResponse.json", 200);
+    PlivoClient client = new PlivoClient("MA123456789012345678", "Zmxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+    Pricing pricing = Pricing.getter("US").client(client)
+      .get();
+
+
+    assertEquals(pricing.getId(), pricing.getCountryIso());
+
+    Map<String, String> params = new LinkedHashMap<>();
+    params.put("country_iso", "US");
+
+    assertRequest("GET", "Pricing/", params);
+  }
 }
