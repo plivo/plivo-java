c8071cff15847db5f4b89956a80501e40e03334d
Abhishek-plivo <abhishek@plivo.com>
2018-04-23 13:39:51 +0530

    CT-82 | add new test cases to for resource.client(client).action() for numbers

--- src/test/java/com/plivo/api/NumberTest.java
+++ src/test/java/com/plivo/api/NumberTest.java
@@ -22,6 +22,17 @@ public class NumberTest extends BaseTest {
   }
 
   @Test
+  public void numberListWithClientShouldSucceed() throws Exception {
+    expectResponse("numberListResponse.json", 200);
+    PlivoClient client = new PlivoClient("MA123456789012345678", "Zmxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+    Number.lister().client(client)
+      .list();
+
+    assertRequest("GET", "Number/");
+  }
+
+  @Test
   public void numberCreateShouldSucceed() throws Exception {
     expectResponse("numberCreateResponse.json", 202);
 
@@ -31,6 +42,16 @@ public class NumberTest extends BaseTest {
     assertRequest("POST", "Number/");
   }
 
+  @Test
+  public void numberCreateWithClientShouldSucceed() throws Exception {
+    expectResponse("numberCreateResponse.json", 202);
+    PlivoClient client = new PlivoClient("MA123456789012345678", "Zmxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+    Number.creator(Collections.singletonList("1231231231"), "carrier", "region").client(client)
+      .create();
+
+    assertRequest("POST", "Number/");
+  }
 
   @Test
   public void numberUpdateShouldSucceed() throws Exception {
@@ -40,6 +61,21 @@ public class NumberTest extends BaseTest {
 
     Number.updater(number)
       .alias("test")
+      .update();
+
+    assertRequest("POST", "Number/%s/", number);
+  }
+
+  @Test
+  public void numberUpdateWithClientShouldSucceed() throws Exception {
+    expectResponse("numberUpdateResponse.json", 202);
+    PlivoClient client = new PlivoClient("MA123456789012345678", "Zmxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+    final String number = "1231231231";
+
+    Number.updater(number)
+      .alias("test")
+      .client(client)
       .update();
 
     assertRequest("POST", "Number/%s/", number);
@@ -61,10 +97,39 @@ public class NumberTest extends BaseTest {
   }
 
   @Test
+  public void numberGetWithClientShouldSucceed() throws Exception {
+    expectResponse("numberGetResponse.json", 202);
+    PlivoClient client = new PlivoClient("MA123456789012345678", "Zmxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+    final String numberNumber = "1231231231";
+
+    Number number = Number.getter(numberNumber).client(client)
+      .get();
+
+
+    assertEquals(number.getId(), number.getNumber());
+
+    assertRequest("GET", "Number/%s/", numberNumber);
+  }
+
+  @Test
   public void phoneNumberListShouldSucceed() throws Exception {
     expectResponse("phoneNumberListResponse.json", 200);
 
     PhoneNumber.lister("US")
+      .list();
+
+    Map<String, String> params = new LinkedHashMap<>();
+    params.put("country_iso", "US");
+    assertRequest("GET", "PhoneNumber/", params);
+  }
+
+  @Test
+  public void phoneNumberListWithClientShouldSucceed() throws Exception {
+    expectResponse("phoneNumberListResponse.json", 200);
+    PlivoClient client = new PlivoClient("MA123456789012345678", "Zmxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+    PhoneNumber.lister("US").client(client)
       .list();
 
     Map<String, String> params = new LinkedHashMap<>();
@@ -79,6 +144,19 @@ public class NumberTest extends BaseTest {
     final String number = "1231231231";
 
     PhoneNumber.creator(number)
+      .create();
+
+    assertRequest("POST", "PhoneNumber/%s/", number);
+  }
+
+  @Test
+  public void phoneNumberCreateWithClientShouldSucceed() throws Exception {
+    expectResponse("phoneNumberCreateResponse.json", 201);
+    PlivoClient client = new PlivoClient("MA123456789012345678", "Zmxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+    final String number = "1231231231";
+
+    PhoneNumber.creator(number).client(client)
       .create();
 
     assertRequest("POST", "PhoneNumber/%s/", number);
