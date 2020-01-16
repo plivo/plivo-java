7312e622af7f339a155400a513dedcbbe2d87111
qyunal <s.kunal@plivo.com>
2020-01-17 02:06:24 +0530

    adds cascade option for delete application API

--- src/main/java/com/plivo/api/models/application/ApplicationDeleter.java
+++ src/main/java/com/plivo/api/models/application/ApplicationDeleter.java
@@ -6,12 +6,30 @@ import retrofit2.Call;
 
 public class ApplicationDeleter extends Deleter<Application> {
 
+  private Boolean cascade;
+
   public ApplicationDeleter(String id) {
     super(id);
+    cascade = true;
+  }
+
+  /**
+   * @return Specify if the subaccount should be cascade deleted or not.
+   */
+  public Boolean cascade() {
+    return this.cascade;
+  }
+
+  /**
+   * @param cascade Specify if the subaccount should be cascade or not.
+   */
+  public ApplicationDeleter cascade(final Boolean cascade) {
+    this.cascade = cascade;
+    return this;
   }
 
   @Override
   protected Call<ResponseBody> obtainCall() {
-    return client().getApiService().applicationDelete(client().getAuthId(), id);
+    return client().getApiService().applicationDelete(client().getAuthId(), id, this);
   }
 }
