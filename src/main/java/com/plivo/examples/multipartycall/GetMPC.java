c2920bb744f2a5e6083f330c1700e5bcd75714ac
qyunal <s.kunal@plivo.com>
2020-07-07 10:53:34 +0530

    adds MultiPartyCallGet

---
+++ src/main/java/com/plivo/examples/multipartycall/GetMPC.java
@@ -0,0 +1,19 @@
+package com.plivo.examples.multipartycall;
+
+import com.plivo.api.Plivo;
+import com.plivo.api.exceptions.PlivoRestException;
+import com.plivo.api.models.multipartycall.MultiPartyCall;
+import com.plivo.api.models.multipartycall.MultiPartyCallUtils;
+
+import java.io.IOException;
+
+public class GetMPC {
+  public static void main(String[] args) throws IOException, PlivoRestException {
+
+    Plivo.init("<YOUR-AUTH-ID>", "YOUR-AUTH-TOKEN");
+
+    // Fetch specific MultiPartyCall
+    MultiPartyCall mpc = MultiPartyCall.getter(MultiPartyCallUtils.mpcUuid("<YOUR-MPC-UUID>")).get();
+    System.out.println((mpc.getMpcUuid().equals("<YOUR-MPC-UUID>")));
+  }
+}
