695a35f292b87c9a994d0b59f8997c938dbe10af
qyunal <s.kunal@plivo.com>
2020-07-07 12:22:36 +0530

    adds MultiPartyCallStop handlers

--- src/main/java/com/plivo/examples/multipartycall/StopMPC.java
+++ src/main/java/com/plivo/examples/multipartycall/StopMPC.java
@@ -7,17 +7,17 @@ import com.plivo.api.models.multipartycall.MultiPartyCallUtils;
 
 import java.io.IOException;
 
-public class StartMPC {
+public class StopMPC {
 
   public static void main(String[] args) throws IOException, PlivoRestException {
     Plivo.init("<YOUR-AUTH-ID>", "<YOUR-AUTH-TOKEN>");
 
-    // You can start an initialized MPC
+    // You can stop an initialized/active MPC
 
-    // 1. either by using static starter() method of MultiPartyCall class
-    MultiPartyCall.starter(MultiPartyCallUtils.friendlyName("myInitializedMPC1")).update();
+    // 1. either by using static stopper() method of MultiPartyCall class
+    MultiPartyCall.stopper(MultiPartyCallUtils.friendlyName("myOngoingMPC1")).delete();
 
     // 2. or calling object method start() of MultiPartyCall object
-    MultiPartyCall.getter(MultiPartyCallUtils.friendlyName("myInitializedMPC2")).get().start();
+    MultiPartyCall.getter(MultiPartyCallUtils.friendlyName("myOngoingMPC2")).get().stop();
   }
 }
