525c9bdfb0847c4a6feb367e185c5d8b72b0ef2f
narayana shanubhogh <narayana@plivo.com>
2020-07-31 22:30:52 +0530

    media upload response fix

--- src/main/java/com/plivo/api/models/media/MediaResponse.java
+++ src/main/java/com/plivo/api/models/media/MediaResponse.java
@@ -3,5 +3,11 @@ package com.plivo.api.models.media;
 import com.plivo.api.models.base.BaseResponse;
 
 public class MediaResponse  extends BaseResponse {
+  private List<MediaUploadResponse> objects;
+
+
+  public List<MediaUploadResponse> getObjects() {
+    return objects;
+  }
 }
 
