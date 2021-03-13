83734bca348ca7e090548e0ceba5531f86a58d85
Renold Jose Thomas <renold@Renolds-MacBook-Pro.local>
2021-03-13 15:27:56 +0530

    powerpack create for adaptive powerpack added

--- src/main/java/com/plivo/api/models/powerpack/PowerpackCreator.java
+++ src/main/java/com/plivo/api/models/powerpack/PowerpackCreator.java
@@ -10,7 +10,8 @@ public class PowerpackCreator extends Creator<PowerpackResponse> {
   private String application_id = null;
   private Boolean sticky_sender = Boolean.TRUE;
   private Boolean local_connect = Boolean.TRUE;
-
+  private NumberPriority[] number_priority;
+  
   PowerpackCreator(String name) {
     if (!Utils.allNotNull(name)) {
       throw new IllegalStateException("powerpack name is required");
@@ -38,7 +39,15 @@ public class PowerpackCreator extends Creator<PowerpackResponse> {
     return this.local_connect;
   }
 
+  public NumberPriority[] number_priority() {
+	    return number_priority;
+	  }
 
+  public PowerpackCreator number_priority(final NumberPriority[] numberPriorities) {
+	    this.number_priority = numberPriorities;
+	    return this;
+	  }
+  
   @Override
   protected Call<PowerpackResponse> obtainCall() {
     return client().getApiService().createPowerpack(client().getAuthId(), this);
