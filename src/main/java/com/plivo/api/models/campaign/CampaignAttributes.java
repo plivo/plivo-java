85d1568216de2da4e8328e927be9032d30d9083a
NirmitiJain <nirmiti.jain@plivo.com>
2022-09-09 10:12:44 +0530

    dev test

--- src/main/java/com/plivo/api/models/campaign/CampaignAttributes.java
+++ src/main/java/com/plivo/api/models/campaign/CampaignAttributes.java
@@ -1,11 +1,10 @@
 package com.plivo.api.models.campaign;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import com.plivo.api.models.base.BaseResource;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class CampaignAttributes extends BaseResource{
+public class CampaignAttributes {
     private Boolean embeddedLink;
     private Boolean embeddedPhone;
     private Boolean ageGated;
