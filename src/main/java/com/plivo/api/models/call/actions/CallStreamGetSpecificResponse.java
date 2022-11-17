184f2f121ff04dd6c313a9442b1926cbc82bc80b
anindya.mahajan <anindya.mahajan@plivo.com>
2022-11-18 00:51:13 +0530

    Added params to Get specific stream response

--- src/main/java/com/plivo/api/models/call/actions/CallStreamGetSpecificResponse.java
+++ src/main/java/com/plivo/api/models/call/actions/CallStreamGetSpecificResponse.java
@@ -7,20 +7,55 @@ import com.plivo.api.models.base.BaseResource;
 @JsonIgnoreProperties({"id"})
 public class CallStreamGetSpecificResponse extends BaseResource {
 
+  private String audioTrack;
+  private String bidirectional;
+  private String billedAmount;
+  private String billedDuration;
   private String callUuid;
+  private String createdAt;
   private String endTime;
+  private String plivoAuthId;
+  private String resourceUri;
   private String serviceUrl;
   private String startTime;
   private String status;
   private String statusCallbackUrl;
   private String streamId;
 
+  public String getAudioTrack() {
+    return audioTrack;
+  }
+
+  public String getBidirectional() {
+    return bidirectional;
+  }
+
+  public String getBilledAmount() {
+    return billedAmount;
+  }
+
+  public String getBilledDuration() {
+    return billedDuration;
+  }
+
   public String getCallUuid() {
     return callUuid;
   }
 
+  public String getCreatedAt() {
+    return createdAt;
+  }
+
   public String getEndTime() {
     return endTime;
+  }
+
+  public String getPlivoAuthId() {
+    return plivoAuthId;
+  }
+
+  public String getResourceUri() {
+    return resourceUri;
   }
 
   public String getServiceUrl() {
