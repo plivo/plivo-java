7fd11131b31972c548295374b4b2588ee9779d5b
Saksham Khandelwal <saksham.khandelwal@plivo.com>
2023-10-13 02:35:32 +0530

    changes

--- src/main/java/com/plivo/api/models/verify/GetVerifiedCallerIdResponse.java
+++ src/main/java/com/plivo/api/models/verify/GetVerifiedCallerIdResponse.java
@@ -3,7 +3,19 @@ package com.plivo.api.models.verify;
 import com.plivo.api.exceptions.PlivoValidationException;
 import com.plivo.api.models.base.BaseResource;
 
+import java.util.Date;
+
 public class GetVerifiedCallerIdResponse extends BaseResource {
+
+  private String alias;
+  private String apiId;
+  private String country;
+  private Date createdAt;
+  private Date modifiedAt;
+  private String phoneNumber;
+  private String subaccount;
+  private String verificationUuid;
+
   @Override
   public String getId() throws PlivoValidationException {
     return null;
--- src/main/java/com/plivo/api/models/verify/UpdateVerifiedCallerIdResponse.java
+++ src/main/java/com/plivo/api/models/verify/UpdateVerifiedCallerIdResponse.java
@@ -2,5 +2,17 @@ package com.plivo.api.models.verify;
 
 import com.plivo.api.models.base.BaseResponse;
 
+import java.util.Date;
+
 public class UpdateVerifiedCallerIdResponse extends BaseResponse {
+
+  private String alias;
+  private String apiId;
+  private String country;
+  private Date createdAt;
+  private Date modifiedAt;
+  private String phoneNumber;
+  private String subaccount;
+  private String verificationUuid;
+
 }
