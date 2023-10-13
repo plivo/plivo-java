7ff7d44881099ab52382b153168273bce78b70f4
Saksham Khandelwal <saksham.khandelwal@plivo.com>
2023-10-13 15:12:10 +0530

    added fields in ListVerifiedCallerId

--- src/main/java/com/plivo/api/models/verify/ListVerifiedCallerId.java
+++ src/main/java/com/plivo/api/models/verify/ListVerifiedCallerId.java
@@ -6,6 +6,52 @@ import retrofit2.Call;
 
 public class ListVerifiedCallerId extends VoiceGetter<ListVerifiedCallerIdResponse> {
 
+  private String country;
+  private String subaccount;
+  private String alias;
+  private int limit;
+  private int offset;
+
+  public String getCountry() {
+    return country;
+  }
+
+  public String getSubaccount() {
+    return subaccount;
+  }
+
+  public String getAlias() {
+    return alias;
+  }
+
+  public int getLimit() {
+    return limit;
+  }
+
+  public int getOffset() {
+    return offset;
+  }
+
+  public ListVerifiedCallerId country(final String country) {
+    this.country = country;
+    return this;
+  }
+  public ListVerifiedCallerId subaccount(final String subaccount) {
+    this.subaccount = subaccount;
+    return this;
+  }
+  public ListVerifiedCallerId alias(final String alias) {
+    this.alias = alias;
+    return this;
+  }
+  public ListVerifiedCallerId limit(final int limit) {
+    this.limit = limit;
+    return this;
+  }
+  public ListVerifiedCallerId offset(final int offset) {
+    this.offset = offset;
+    return this;
+  }
 
   public ListVerifiedCallerId() {
     super("");
